f9d03aaaba19bd548de7e9de387288e784b9eefe
andrenguyener <andrenguyenp@gmail.com>
2021-12-24 21:00:28 -0800

    feat: add loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 
-import { useColorMode, Box, VStack, HStack, Heading, Text } from "@chakra-ui/react";
+import { useColorMode, Box, VStack, HStack, Heading, Text, Spinner, Fade } from "@chakra-ui/react";
 import { OrbitControls, Environment } from "@react-three/drei";
 import { Canvas, useThree } from "@react-three/fiber";
 import { useControls, Leva } from "leva";
@@ -8,21 +8,25 @@ import { useControls, Leva } from "leva";
 import { Lights } from "../components/Lights";
 import { Model } from "../components/Model";
 
-const SetCamera = () => {
+const UpdateCamera = () => {
     const [initialLoaded, setInitialLoaded] = React.useState(false);
-
     React.useEffect(() => {
         setInitialLoaded(true);
     }, []);
 
     useThree(({ camera, scene }) => {
-        (camera as THREE.PerspectiveCamera).aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+        const currentAspect = (camera as THREE.PerspectiveCamera).aspect;
+        const newAspect = window.innerWidth / window.innerHeight;
+        if (currentAspect !== newAspect) {
+            (camera as THREE.PerspectiveCamera).aspect = newAspect;
+            camera.updateProjectionMatrix();
+        }
         if (!initialLoaded) {
             scene.position.y = -2;
             camera.manual = true;
         }
     });
+
     return null;
 };
 
@@ -37,8 +41,72 @@ const MODELS = {
     },
 };
 
+const SetLoading = ({
+    setIsLoading,
+}: {
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+    React.useEffect(() => {
+        return () => {
+            setIsLoading(false);
+        };
+    }, []);
+
+    return null;
+};
+
+const LoadingSpinner = ({ isIn }: { isIn: boolean }) => {
+    return (
+        <Fade
+            in={isIn}
+            transition={{
+                exit: {
+                    duration: 1,
+                },
+            }}
+        >
+            <Box
+                position="fixed"
+                w="100%"
+                h="100%"
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Spinner size="xl" />
+            </Box>
+        </Fade>
+    );
+};
+
+const Header = () => {
+    return (
+        <Box w="100%" p={4} background="transparent">
+            Voxel Blox
+        </Box>
+    );
+};
+
+const Cards = ({ height }: { height: string }) => {
+    return (
+        <Box w="100%" h={height} p={4}>
+            <HStack spacing={2} justify="center">
+                <Feature
+                    title="Plan Money"
+                    desc="The future can be even brighter but a goal without a plan is just a wish"
+                />
+                <Feature
+                    title="Save Money"
+                    desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
+                />
+            </HStack>
+        </Box>
+    );
+};
+
 const Home = () => {
     const { colorMode, setColorMode } = useColorMode();
+    const [isLoading, setIsLoading] = React.useState(true);
 
     const { model, rotate } = useControls({
         darkTheme: {
@@ -80,92 +148,84 @@ const Home = () => {
     );
 
     return (
-        <Box height={"100vh"} width={"100vw"} transition="0.5s ease-out" overflow={"hidden"}>
-            <VStack
-                // divider={<StackDivider borderColor="gray.200" />}
-                spacing={3}
-                direction="column"
-                align="stretch"
-                height={"100%"}
-            >
-                <Box w="100%" p={4} background="transparent">
-                    Voxel Blox
-                </Box>
-                <Canvas
-                    shadows={true}
-                    dpr={[1, 2]}
-                    camera={{
-                        position: [6, 2, 9],
-                        fov: 100,
+        <Box h="100vh" w="100vw" transition="0.5s ease-out" overflow={"hidden"}>
+            <LoadingSpinner isIn={isLoading} />
+            <VStack spacing={3} direction="column" align="stretch" h="100%">
+                <Header />
+                <Fade
+                    in={!isLoading}
+                    transition={{
+                        enter: {
+                            duration: 1,
+                        },
+                    }}
+                    style={{
+                        height: "100%",
+                        width: "100%",
                     }}
                 >
-                    <Suspense fallback={null}>
-                        <fog attach="fog" args={["#17171b", 30, 40]} />
+                    <Canvas
+                        shadows={true}
+                        camera={{
+                            position: [6, 2, 9],
+                            fov: 85,
+                        }}
+                    >
+                        <Suspense fallback={<SetLoading setIsLoading={setIsLoading} />}>
+                            <fog attach="fog" args={["#17171b", 30, 40]} />
+                            <UpdateCamera />
+                            <mesh receiveShadow={true} rotation={[-Math.PI / 2, 0, 0]}>
+                                <planeBufferGeometry args={[250, 250]} />
+                                <shadowMaterial
+                                    color={colorMode === "light" ? "#464343" : "#000"}
+                                    transparent={true}
+                                />
+                            </mesh>
 
-                        <SetCamera />
-                        <mesh receiveShadow={true} rotation={[-Math.PI / 2, 0, 0]}>
-                            <planeBufferGeometry args={[250, 250]} />
-                            <shadowMaterial
-                                color={colorMode === "light" ? "#464343" : "#000"}
-                                transparent={true}
+                            <Lights
+                                ambientLight={{
+                                    color: lighting.color,
+                                    intensity: lighting.intensity,
+                                }}
+                                directionalLight={{
+                                    castShadow: lighting.castShadow,
+                                    intensity: 1,
+                                    position: [10, 10, 6],
+                                    shadowRadius: 100,
+                                }}
                             />
-                        </mesh>
+                            {debug.grid && (
+                                <>
+                                    <gridHelper
+                                        args={[10, 10, 0x006600, 0x006600]}
+                                        position={[0, 0, 0]}
+                                    />
+                                    <gridHelper
+                                        args={[10, 10, 0x000066, 0x000066]}
+                                        position={[0, 5, -5]}
+                                        rotation={[Math.PI / 2, 0, 0]}
+                                    />
+                                    <gridHelper
+                                        args={[10, 10, 0x660000, 0x660000]}
+                                        position={[-5, 5, 0]}
+                                        rotation={[0, 0, Math.PI / 2]}
+                                    />
+                                </>
+                            )}
+                            <OrbitControls autoRotate={rotate} />
 
-                        <Lights
-                            ambientLight={{
-                                color: lighting.color,
-                                intensity: lighting.intensity,
-                            }}
-                            directionalLight={{
-                                castShadow: lighting.castShadow,
-                                intensity: 1,
-                                position: [10, 10, 6],
-                                shadowRadius: 100,
-                            }}
-                        />
-                        {debug.grid && (
-                            <>
-                                <gridHelper
-                                    args={[10, 10, 0x006600, 0x006600]}
-                                    position={[0, 0, 0]}
-                                />
-                                <gridHelper
-                                    args={[10, 10, 0x000066, 0x000066]}
-                                    position={[0, 5, -5]}
-                                    rotation={[Math.PI / 2, 0, 0]}
-                                />
-                                <gridHelper
-                                    args={[10, 10, 0x660000, 0x660000]}
-                                    position={[-5, 5, 0]}
-                                    rotation={[0, 0, Math.PI / 2]}
-                                />
-                            </>
-                        )}
-                        <OrbitControls autoRotate={rotate} />
-
-                        <Model
-                            mtlPath={MODELS[model].mtl}
-                            objPath={MODELS[model].obj}
-                            options={{
-                                wireframe: debug.wireframe,
-                            }}
-                        />
-
-                        <Environment preset="dawn" />
-                    </Suspense>
-                </Canvas>
-                <Box w="100%" height="300px" p={4}>
-                    <HStack spacing={2} justify="center">
-                        <Feature
-                            title="Plan Money"
-                            desc="The future can be even brighter but a goal without a plan is just a wish"
-                        />
-                        <Feature
-                            title="Save Money"
-                            desc="You deserve good things. With a whooping 10-15% interest rate per annum, grow your savings."
-                        />
-                    </HStack>
-                </Box>
+                            <Model
+                                mtlPath={MODELS[model].mtl}
+                                objPath={MODELS[model].obj}
+                                options={{
+                                    wireframe: debug.wireframe,
+                                }}
+                            />
+                            <Environment preset="dawn" />
+                        </Suspense>
+                    </Canvas>
+                </Fade>
+                <Cards height={"300px"} />
                 <Leva
                     flat={true}
                     titleBar={{
@@ -173,6 +233,7 @@ const Home = () => {
                         filter: false,
                         title: "Controls",
                     }}
+                    hideCopyButton={true}
                 />
             </VStack>
         </Box>
